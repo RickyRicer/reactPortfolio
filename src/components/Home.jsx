7e9f723fb5a99051de62a20787ba33b53c9cd0da
Rick Ansay <rick.ansay@gmail.com>
2024-05-03 17:20:44 -0600

    updated carousel'
    .

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -12,7 +12,7 @@ export const Home = () => {
         <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
             <p className="text-yellow-300">{("Hello there, my name is")}</p>
             <h1 className="text-4xl sm:text-7xl font-bold text-[#ccd6f6]">Rick Ansay</h1>
-            <h2 className="text-4xl sm:text-7xl font-bold text-[#8892b0]"><Typewriter options={{strings: ["I'm a Full Stack Developer.", "I'm a problem solver.", "I'm a scuba diver.", "I'm crazy about NFTs."], autoStart: true, loop: true}} /></h2>
+            <h2 className="text-4xl sm:text-7xl font-bold text-[#8892b0]"><Typewriter options={{strings: ["I'm a Full Stack Developer.", "I'm a problem solver.", "I'm a scuba diver.", "I'm crazy about crypto."], autoStart: true, loop: true}} /></h2>
             <p className="text-[#8892b0] py-4 max-w-[700px]">{("Welcome to my portfolio. I look forward to helping you achieve your next project's goal! I'm always challenging myself to learn new things! Click below to see my portfolio, or download my resume!")}</p>
             <div>
                 <Link to="projects" smooth={true} duration={500}>

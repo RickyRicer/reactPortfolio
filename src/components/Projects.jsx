e83877356a7efdaa0354e394031e99ab55937b11
Rick Ansay <rick.ansay@gmail.com>
2024-09-18 20:22:53 -0600

    Updated projects section notes

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -10,7 +10,7 @@ export const Projects = () => {
       <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
         <div className="pb-8">
           <p className="text-4xl font-bold inline border-b-4 border-yellow-300 text-gray-300">{("Projects")}</p>
-          <p className="py-6">{("Some of the great projects I've helped develop! View the demo or github for more information.")}</p>
+          <p className="py-6">{("This section is currently being reworked as I update broken links/hosting sites. The code links are still valid! Some of the great projects I've helped develop! View the demo or github for more information.")}</p>
         </div>
 		<IndividualProject />
       </div>

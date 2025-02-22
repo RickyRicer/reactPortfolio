1b8c8f287bbbfb7909b2f12f378708b2bc612fe1
Rick Ansay <rick.ansay@gmail.com>
2025-02-21 23:40:14 -0700

    Replaced naming convention for code with repo

--- src/components/IndividualProject.jsx
+++ src/components/IndividualProject.jsx
@@ -12,13 +12,13 @@ import Weather from '../assets/weather.png';
 export const IndividualProject = () => {
 
   const projects = [
-    { id: 1, name: "AutoSprink", img: AutoSprink, demo: 'https://autosprink.com/', code: 'https://github.com/mepcad-ransay', desc: 'Multiple apps that utilize MithrilJS, a React-like front end. Make sure to view other products'},
-    { id: 2, name: "MoonBay", img: MoonBay, demo: 'https://moonbay.herokuapp.com/', code: 'https://github.com/theresaqueryforthat/MoonBay', desc: 'App using the OpenSea API to create a WEB2 NFT site'},
-    { id: 3, name: "NamePassPro", img: NamePassPro, demo: 'https://namepass.herokuapp.com/', code: 'https://github.com/theresaqueryforthat/NamePassPro', desc: 'A password and username randomizer app, built using MVC' },
-    { id: 4, name: "Tech Blog", img: Blog, demo: 'https://tech-feed-rb.herokuapp.com/', code: 'https://github.com/RickyRicer/techBlogMVC', desc: 'App for blogging, another MVC framework' },
-    { id: 5, name: "NamePass", img: NamePass, demo: 'https://taqft.github.io/namepass/', code: 'https://github.com/taqft/namepass', desc: 'A basic version of NamePassPro' },
-    { id: 6, name: "Employee Tracker", img: EmployeeTracker, demo: '#', code: 'https://github.com/RickyRicer/employeeTracker', desc: 'A CLI app that utilizes mySQL, inquirer.js, and C Table to create an employee database'},
-    { id: 7, name: "Weather Dashboard", img: Weather, demo: 'https://rickyricer.github.io/Weather-Dashboard-Using-APIs/', code: 'https://github.com/RickyRicer/Weather-Dashboard-Using-APIs', desc: 'A weather app connected to Weather API'}
+    { id: 1, name: "AutoSprink", img: AutoSprink, demo: 'https://autosprink.com/', repo: 'https://github.com/mepcad-ransay', desc: 'Multiple apps that utilize MithrilJS, a React-like front end. Make sure to view other products'},
+    { id: 2, name: "MoonBay", img: MoonBay, demo: 'https://moonbay.herokuapp.com/', repo: 'https://github.com/theresaqueryforthat/MoonBay', desc: 'App using the OpenSea API to create a WEB2 NFT site'},
+    { id: 3, name: "NamePassPro", img: NamePassPro, demo: 'https://namepass.herokuapp.com/', repo: 'https://github.com/theresaqueryforthat/NamePassPro', desc: 'A password and username randomizer app, built using MVC' },
+    { id: 4, name: "Tech Blog", img: Blog, demo: 'https://tech-feed-rb.herokuapp.com/', repo: 'https://github.com/RickyRicer/techBlogMVC', desc: 'App for blogging, another MVC framework' },
+    { id: 5, name: "NamePass", img: NamePass, demo: 'https://taqft.github.io/namepass/', repo: 'https://github.com/taqft/namepass', desc: 'A basic version of NamePassPro' },
+    { id: 6, name: "Employee Tracker", img: EmployeeTracker, demo: '#', repo: 'https://github.com/RickyRicer/employeeTracker', desc: 'A CLI app that utilizes mySQL, inquirer.js, and C Table to create an employee database'},
+    { id: 7, name: "Weather Dashboard", img: Weather, demo: 'https://rickyricer.github.io/Weather-Dashboard-Using-APIs/', repo: 'https://github.com/RickyRicer/Weather-Dashboard-Using-APIs', desc: 'A weather app connected to Weather API'}
   ];
 
   return (
@@ -32,7 +32,7 @@ export const IndividualProject = () => {
                 <a href={project.demo} >
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">Demo</button>
                 </a>
-                <a href={project.code}>
+                <a href={project.repo}>
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">Repo</button>
                 </a>
               </div>
